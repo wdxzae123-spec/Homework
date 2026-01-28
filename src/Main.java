4e7262f5ebde73133a999d5219761284d593c3d6
Xorset <wdxzae123@mail.ru>
2026-01-28 21:23:24 +0500

    1st work

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println("Dog= " + dog);
-        System.out.println("Cat= " + cat);
-        System.out.println("Paper= " + paper);
+        System.out.println("Dog=" + dog);
+        System.out.println("Cat=" + cat);
+        System.out.println("Paper=" + paper);
     }
 }
 
