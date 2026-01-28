779776c8bb889e2c7cca13ec86508c6c3bdcb601
Xorset <wdxzae123@mail.ru>
2026-01-28 21:43:39 +0500

    5st work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-
 public class Main {
     public static void main(String[] args) {
-        var friend = 19;
-        var Friend = (friend + 2) / 7;
-        System.out.println("Friend=" + Friend);
+        var frog = 3.5;
+        var Frog1 = frog * 2;
+        var Frog2 = Frog1 / 7;
+        var Frog3 = Frog2 + 4;
+        System.out.println("Frog=" + frog);
+        System.out.println("Frog1=" + Frog1);
+        System.out.println("Frog2=" + Frog2);
+        System.out.println("Frog3=" + Frog3);
 
     }
 }
