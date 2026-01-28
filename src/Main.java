86bfb95aafee566eb7d6fa3f466fbdba2d1ae981
Xorset <wdxzae123@mail.ru>
2026-01-28 22:04:56 +0500

    7st work

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
         var fighter1 = 78.2;
         var fighter2 = 82.7;
         var generalMass = fighter1 + fighter2;
-        var differensMass = fighter2 - fighter1;
+        var differensMass = fighter1 - fighter2;
+        var overload = fighter2 % fighter1;
         System.out.println("Общая масса=" + generalMass);
         System.out.println("Разница в массе=" + differensMass);
+        System.out.println("Остаток=" + overload);
     }
 }
 
