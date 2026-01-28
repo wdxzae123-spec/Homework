d8db5487634f08a6a968f990428ed99b59974675
Xorset <wdxzae123@mail.ru>
2026-01-28 21:32:45 +0500

    3st work

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        var Dog = dog + 4;
-        var Cat = cat + 4;
-        var Paper = paper + 4;
+        var Dog = dog + 4 - 3.5;
+        var Cat = cat + 4 - 1.6;
+        var Paper = paper + 4 - 7639;
 
         System.out.println("Dog=" + Dog);
         System.out.println("Cat=" + Cat);
