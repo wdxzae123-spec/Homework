99e5b6859260ced813ea1c283a3b86694a267bf9
Xorset <wdxzae123@mail.ru>
2026-01-28 21:17:55 +0500

    1st works

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Dog= " + dog);
+        System.out.println("Cat= " + cat);
+        System.out.println("Paper= " + paper);
     }
 }
+
